73e906fc5343995a11d90351796ed7016dcad326
damian nardini <damiannardini@gmail.com>
2020-11-30 08:08:59 +0100

    styling for color cards

--- src/components/ColorCard.vue
+++ src/components/ColorCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="color-card">
+  <div class="color-card" :style="{ backgroundColor: this.colorHex }">
     <span class="color-card__id">{{colorId}}</span>
     <span class="color-card__name">{{colorName}}</span>
     <span class="color-card__hex">{{colorHex}}</span>
@@ -20,4 +20,30 @@ export default {
 </script>
 
 <style lang="scss">
+  .color-card {
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    background: white;
+    border-radius: 0.5rem;
+    min-width: 8rem;
+    width: calc(100%/3);
+    box-shadow: 0 0 0.3rem rgba(0,0,0,0.4);
+    color: white;
+    text-shadow: 0 0 0.1rem rgba(0,0,0,1);
+    font-size: 2rem;
+    padding: 1rem;
+  }
+  .color-card__id {
+    align-self: flex-start;
+  }
+  .color-card__name {
+    line-height: 4rem;
+  }
+  .color-card__hex {
+    line-height: 4rem;
+  }
+  .color-card__pantone {
+    align-self: flex-end;
+  }
 </style>

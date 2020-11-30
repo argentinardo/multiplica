c3ccf56c2d88e901cdd51a119892b391011e258e
damian nardini <damiannardini@gmail.com>
2020-11-30 07:48:31 +0100

    json parsed added

--- src/components/ColorContainer.vue
+++ src/components/ColorContainer.vue
@@ -1,10 +1,33 @@
 <template>
-  <color-card></color-card>
+  <div class="color-container">
+    <color-card
+     v-for="color in colors.data.data"
+     :key="color.id"
+     :colorId="color.id"
+     :colorName="color.name"
+     :colorHex="color.color"
+     :colorPantone="color.pantone_value"
+     >
+    </color-card>
+  </div>
 </template>
 
 <script>
 import ColorCard from './ColorCard.vue'
+  import axios from 'axios'
 export default {
+  data() {
+   return {
+    jsonUrl: "https://reqres.in/api/colors",
+    colors: []
+   }
+  },
+  mounted(){
+    axios
+      .get( this.jsonUrl)
+      .then( response => (this.colors = response))
+      .catch( error => console.log(error))
+  },
   components: { ColorCard },
   name: 'ColorContainer',
   props: {
@@ -14,5 +37,9 @@ export default {
 </script>
 
 <style lang="scss">
-
+  .color-container {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  }
 </style>
